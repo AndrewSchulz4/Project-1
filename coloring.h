37a291644af5a9aafb1a14fd692900e3921f72ab
JohnKrossa <jtkrossa@gmail.com>
2021-11-18 15:06:33 -0800

    set rho to 0 - fixed weird lighting
    Specular lighting seems to be implemented correctly but I can't tell if what we get when rho > 0 is what we are supposed to be seeing. It might be right? It looks odd so I set it too 0

--- coloring.h
+++ coloring.h
@@ -3,7 +3,7 @@
     GLfloat constantAttenuation = 0.5f;
     GLfloat linearAttenuation = 0.1f;
     GLfloat quadraticAttenuation = 0.05f;
-    GLfloat rho = 0.5f;
+    GLfloat rho = 0.0f; //causes sphere weird shading when above 0
 
     GLfloat dist = distance(point.getPosition(), l.getPosition());    
 
@@ -11,7 +11,7 @@
     Ray toLight(point.getPosition(), l.getPosition());
     Ray toCamera(point.getPosition(), c.getPosition());
     Ray reflectedRay(point.getPosition(), reflect(toLight.getDirection(), point.getNormal()));
-    glm::vec3 color3 = ((point.materialv().get_k_a() * point.materialv().get_I_a() + (totalAttenuation * point.materialv().get_k_d() * point.materialv().get_I_d() * max(0.0f, dot(toLight.getDirection(), point.getNormal()))) + (totalAttenuation * point.materialv().get_k_s() * point.materialv().get_I_s() * pow(max(0.0f, dot(reflectedRay.getDirection(), toCamera.getDirection())), rho))));
+    glm::vec3 color3 = ((point.materialv().get_k_a() * point.materialv().get_I_a() + (totalAttenuation * point.materialv().get_k_d() * point.materialv().get_I_d() * max(0.0f, dot(toLight.getDirection(), point.getNormal()))) + (totalAttenuation * point.materialv().get_k_s() * point.materialv().get_I_s() * pow(max(0.0f, dot(toCamera.getDirection(), reflectedRay.getDirection())), rho))));
 
     glm::vec4 color4 = {color3[0], color3[1], color3[2], 1.0f};
     return color4;
